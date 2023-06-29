cd5edc06e8968104f151a4d7e54e2a8dd128f50c
Duncan Stephenson <dstephenson14@ivytech.edu>
2023-06-29 14:35:34 -0400

    Loop
    Added loop feature to game

--- Main.cpp
+++ Main.cpp
@@ -8,53 +8,65 @@ std::uniform_int_distribution<int> dist(1, 3); // Distribution of random numbers
 
 int main() {
   std::string choice;//player's choice
+  bool playAgain = true;
   
   std::cout << "Welcome to Rock, Paper, Scissors!\n";
-  std::cout << "Please enter your choice: ";
-  std::cin >> choice;
   
-  //displays player's choice
-  std::cout << "You chose " << choice << "!\n";
-  
-  std::string choices[3] = {"Rock", "Paper", "Scissors"};//array of choices
-  
-  int choice_num = dist(rng);//random number between 1 and 3
-  
-  //displayers computer's choice (subtract 1 to get the index of the choice)
-  std::cout << "The computer chose " << choices[choice_num - 1] << "!\n";
+  while (playAgain) {
+    std::cout << "Please enter your choice (or 'exit' to quit): ";
+    std::cin >> choice;
 
-  //makes choice lowercase
-  std::transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
-
-  //Displays outcome of game based on player and computer choices
-  if (choice == "rock" && choice_num == 1) {
-    std::cout << "It's a tie!\n";
+    if (choice == "exit") {
+      playAgain = false;
+      break;
+    }
   
-  } else if (choice == "rock" && choice_num == 2) {
-      std::cout << "You lose!\n";
-    
-  } else if (choice == "rock" && choice_num == 3) {
-      std::cout << "You win!\n";
-    
-  } else if (choice == "paper" && choice_num == 1) {
-      std::cout << "You win!\n";
-    
-  } else if (choice == "paper" && choice_num == 2) {
+    //displays player's choice
+    std::cout << "You chose " << choice << "!\n";
+  
+    std::string choices[3] = {"Rock", "Paper", "Scissors"};//array of choices
+  
+    int choice_num = dist(rng);//random number between 1 and 3
+  
+    //displayers computer's choice (subtract 1 to get the index of the choice)
+    std::cout << "The computer chose " << choices[choice_num - 1] << "!\n";
+
+    //makes choice lowercase
+    std::transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
+
+    //Displays outcome of game based on player and computer choices
+    if (choice == "rock" && choice_num == 1) {
       std::cout << "It's a tie!\n";
     
-  } else if (choice == "paper" && choice_num == 3) {
-      std::cout << "You lose!\n";
-    
-  } else if (choice == "scissors" && choice_num == 1) {
-      std::cout << "You lose!\n";
-    
-  } else if (choice == "scissors" && choice_num == 2) {
-      std::cout << "You win!\n";
-    
-  } else if (choice == "scissors" && choice_num == 3) {
-      std::cout << "It's a tie!\n";
-    
-  } else {
-      std::cout << "Invalid choice. Please try again.\n";
+    } else if (choice == "rock" && choice_num == 2) {
+        std::cout << "You lose!\n";
+      
+    } else if (choice == "rock" && choice_num == 3) {
+        std::cout << "You win!\n";
+      
+    } else if (choice == "paper" && choice_num == 1) {
+        std::cout << "You win!\n";
+      
+    } else if (choice == "paper" && choice_num == 2) {
+        std::cout << "It's a tie!\n";
+      
+    } else if (choice == "paper" && choice_num == 3) {
+        std::cout << "You lose!\n";
+      
+    } else if (choice == "scissors" && choice_num == 1) {
+        std::cout << "You lose!\n";
+      
+    } else if (choice == "scissors" && choice_num == 2) {
+        std::cout << "You win!\n";
+      
+    } else if (choice == "scissors" && choice_num == 3) {
+        std::cout << "It's a tie!\n";
+      
+    } else {
+        std::cout << "Invalid choice. Please try again.\n";
+    }
+  
+    std::cout << "\n";
   }
 }
+
