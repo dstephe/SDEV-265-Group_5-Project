ca58e4f7de71d857ea232c71843556b6010eeb34
Duncan Stephenson <dstephenson14@ivytech.edu>
2023-07-17 17:14:20 -0400

    Update Main.cpp
    added scutterbotch11's code

--- Main.cpp
+++ Main.cpp
@@ -1,72 +1,20 @@
+#include "RockPaperScissors.h"
+#include <cstdlib>
 #include <iostream>
-#include <random>
-#include <algorithm>
+#include <sstream>
+#include <string>
 
-std::random_device rd;     //used once to initialise
-std::mt19937 rng(rd());    // Random-number engine Mersenne-Twister
-std::uniform_int_distribution<int> dist(1, 3); // Distribution of random numbers
+using namespace System;
+using namespace System::Windows::Forms;
+[STAThread]
 
-int main() {
-  std::string choice;//player's choice
-  bool playAgain = true;
-  
-  std::cout << "Welcome to Rock, Paper, Scissors!\n";
-  
-  while (playAgain) {
-    std::cout << "Please enter your choice (or 'exit' to quit): ";
-    std::cin >> choice;
 
-    if (choice == "exit") {
-      playAgain = false;
-      break;
-    }
-  
-    //displays player's choice
-    std::cout << "You chose " << choice << "!\n";
-  
-    std::string choices[3] = {"Rock", "Paper", "Scissors"};//array of choices
-  
-    int choice_num = dist(rng);//random number between 1 and 3
-  
-    //displayers computer's choice (subtract 1 to get the index of the choice)
-    std::cout << "The computer chose " << choices[choice_num - 1] << "!\n";
+void main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Projectpractice::RockPaperScissors form;
+	Application::Run(% form);
 
-    //makes choice lowercase
-    std::transform(choice.begin(), choice.end(), choice.begin(), ::tolower);
-
-    //Displays outcome of game based on player and computer choices
-    if (choice == "rock" && choice_num == 1) {
-      std::cout << "It's a tie!\n";
-    
-    } else if (choice == "rock" && choice_num == 2) {
-        std::cout << "You lose!\n";
-      
-    } else if (choice == "rock" && choice_num == 3) {
-        std::cout << "You win!\n";
-      
-    } else if (choice == "paper" && choice_num == 1) {
-        std::cout << "You win!\n";
-      
-    } else if (choice == "paper" && choice_num == 2) {
-        std::cout << "It's a tie!\n";
-      
-    } else if (choice == "paper" && choice_num == 3) {
-        std::cout << "You lose!\n";
-      
-    } else if (choice == "scissors" && choice_num == 1) {
-        std::cout << "You lose!\n";
-      
-    } else if (choice == "scissors" && choice_num == 2) {
-        std::cout << "You win!\n";
-      
-    } else if (choice == "scissors" && choice_num == 3) {
-        std::cout << "It's a tie!\n";
-      
-    } else {
-        std::cout << "Invalid choice. Please try again.\n";
-    }
-  
-    std::cout << "\n";
-  }
 }
 
